a1428a2ca444e5a0d6ee64bf7e47289317f04e7d
zhugeyicixin <hetanjin@163.com>
2016-04-17 14:21:09 +0800

    add the function to convert .gjf file to .mol file
    just need to use counterA.mole.generateMOLFile() as the test.py file

--- demoForLocal/python/test.py
+++ demoForLocal/python/test.py
@@ -1,4 +1,5 @@
 import groupCounter
+import os
 
 counterA = groupCounter.groupCounter()
 
@@ -7,12 +8,31 @@ counterA = groupCounter.groupCounter()
 # counterA.readGroupTemplate()
 # counterA.writeDBGCVector(overwrite=False)
 
-# counterA.readGjfFile(fileName='C5H10_5.gjf', directory='Gjfs', moleculeLabel='test1')
-# counterA.readGroupTemplate()
-# counterA.writeDBGCVector(overwrite=True)
+counterA.readGjfFile(fileName='banana_090.gjf', directory='Gjfs',moleculeLabel='testmole')
+counterA.readGroupTemplate()
+counterA.writeDBGCVector(overwrite=True)
+counterA.mole.generateMOLFile()
 
-for i in xrange(1,28):
-	print "%03d" % i
-	counterA.readGjfFile(fileName='apple_' + '%03d'%i + '.gjf', directory='Gjfs', moleculeLabel='test'+'%03d'%i)
-	counterA.readGroupTemplate()
-	counterA.writeDBGCVector(overwrite=False)
+# for i in xrange(1,28):
+# 	print "%03d" % i
+# 	counterA.readGjfFile(fileName='apple_' + '%03d'%i + '.gjf', directory='Gjfs', moleculeLabel='test'+'%03d'%i)
+# 	counterA.readGroupTemplate()
+# 	counterA.writeDBGCVector(overwrite=False)
+
+# toCalc = range(1,98)
+# for i in toCalc:
+# 	print "%03d" % i
+# 	if os.path.exists(os.path.join('Gjfs', 'banana_' + '%03d'%i + '.gjf')):	
+# 		counterA.readGjfFile(fileName='banana_' + '%03d'%i + '.gjf', directory='Gjfs', moleculeLabel='banana_'+'%03d'%i)
+# 		counterA.readGroupTemplate()
+# 		counterA.writeDBGCVector(overwrite=False)
+# 	else:
+# 		print 'banana_' + '%03d'%i + '.gjf does not exist'
+
+
+# for i in xrange(80,81):
+# 	print "%03d" % i
+# 	counterA.readGjfFile(fileName='banana_' + '%03d'%i + '.gjf', directory='Gjfs', moleculeLabel='banana_'+'%03d'%i)
+# 	counterA.readGroupTemplate()
+# 	counterA.writeDBGCVector(overwrite=False)
+
